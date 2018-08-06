f048d0145af7bc7aabde2fdcc9b979dfa42fcfc4
michstei <steinerm1991@gmail.com>
2018-08-06 15:51:39 +0200

    keras test 2

--- code/python/keras_test.py
+++ code/python/keras_test.py
@@ -5,19 +5,25 @@ from keras.models import Model
 import numpy as np
 import os
 model = VGG16(weights='imagenet')
-
-layer_name = 'block5_conv3'
+layer_name = 'predictions'
 intermediate_model = Model(inputs=model.input, outputs=model.get_layer(layer_name).output )
 img_path = '../../data/Medico_2018_development_set/colon-clear/1.jpg'
-print(img_path)
 img = image.load_img(img_path, target_size=(224, 224))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
 
-preds = model.predict(x)
+# preds = model.predict(x)
 
-print('predicted: ', decode_predictions(preds,top=3)[0])
+# print('predicted: ', decode_predictions(preds,top=3)[0])
 preds = intermediate_model.predict(x)
-
-print('predicted: ', preds)
+for x1 in preds:
+    # for x2 in x1:
+    #     for x3 in x2:
+    #         print('x3',len(x3))
+    #         break
+    #     print('x2',len(x2))
+    #     break
+    print('x1',len(x1))
+    break
+print('preds',len(preds))
