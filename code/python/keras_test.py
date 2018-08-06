88a1ae8fd63955b4245bd4685b6ced3fc2553850
michstei <steinerm1991@gmail.com>
2018-08-06 13:11:31 +0200

    keras test

---
+++ code/python/keras_test.py
@@ -0,0 +1,23 @@
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+from keras.models import Model
+import numpy as np
+import os
+model = VGG16(weights='imagenet')
+
+layer_name = 'block5_conv3'
+intermediate_model = Model(inputs=model.input, outputs=model.get_layer(layer_name).output )
+img_path = '../../data/Medico_2018_development_set/colon-clear/1.jpg'
+print(img_path)
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+
+print('predicted: ', decode_predictions(preds,top=3)[0])
+preds = intermediate_model.predict(x)
+
+print('predicted: ', preds)
