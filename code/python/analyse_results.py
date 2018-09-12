5892cb0b528e82c10f9de3b052c54bb0e215df3c
michstei <steinerm1991@gmail.com>
2018-09-12 14:42:16 +0200

    started with script to anakyse the result files

---
+++ code/python/analyse_results.py
@@ -0,0 +1,14 @@
+
+class Result():
+    
+    def __init__(self,filename):
+        self.total = 0
+        self.correct = 0
+        self.incorrect = 0
+        self.correct_percent = 0.0
+        self.filename = filename
+        self.parse()
+
+    def parse(self):
+        pass
+            
